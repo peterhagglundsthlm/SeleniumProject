c45ca3800786b75654e334ce7c4c34bb2064ac90
Kristofer Klarin <kristofer.klarin@vr.se>
2019-07-22 15:58:35 +0200

    Jag har skapat nya metoder som räknar ut tiden det tar att köra testfall och testsuite. De ligger nu i egna klasser och kallas av BeforeAfterTest

--- src/driverAndCommands/EndTime.java
+++ src/driverAndCommands/EndTime.java
@@ -1,18 +1,45 @@
 package driverAndCommands;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.openqa.selenium.WebElement;
 
 
 public class EndTime {
-	
-	
-	public static WebElement EndTimeSuite(long durationSuite, long startTimeSuite) {
+
+
+	public static WebElement EndTimeSuite(double durationSuite, double startTimeSuite) {
 
 		durationSuite = System.currentTimeMillis() - startTimeSuite;
-		float sekunder = durationSuite/1000;
-		float minuter = sekunder/60;
-		System.out.println("Denna testsuite tog " + minuter + " minuter att genomföra" );
+		double inputSekunder = durationSuite/1000;
+		double inputminuter = inputSekunder/60;
+
+		BigDecimal sekund = new BigDecimal(inputSekunder).setScale(2, RoundingMode.HALF_UP);
+		double sekunder = sekund.doubleValue();
+
+		BigDecimal minut = new BigDecimal(inputminuter).setScale(2, RoundingMode.HALF_UP);
+		double minuter = minut.doubleValue();
+
+		System.out.println("Denna testsuite tog " + sekunder + " sekunder att genomföra vilket motsvarar ca " + minuter + " minuter");
+
+		return null;
+	}
+
+	public static WebElement EndTimeTest(double durationTest, double startTimeTest) {
+
+		durationTest = System.currentTimeMillis() - startTimeTest;
+		double inputSekunder = durationTest/1000;
+		double inputminuter = inputSekunder/60;
+
+		BigDecimal sekund = new BigDecimal(inputSekunder).setScale(2, RoundingMode.HALF_UP);
+		double sekunder = sekund.doubleValue();
+
+		BigDecimal minut = new BigDecimal(inputminuter).setScale(2, RoundingMode.HALF_UP);
+		double minuter = minut.doubleValue();
+		System.out.println("Detta testfall tog " + sekunder + " sekunder att genomföra vilket motsvarar ca " + minuter + " minuter");
+
 		return null;
 	}
 }
